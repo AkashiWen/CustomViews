fffb224a66d5350ee70b33fc1de5912ab0a33198
Akashiwen <343611864@qq.com>
2022-04-27 02:33:38 +0800

    feat: 加载png实现双击缩放

---
+++ app/src/main/java/com/akashi/customviews/acivity/ScalableActivity.kt
@@ -0,0 +1,14 @@
+package com.akashi.customviews.acivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.akashi.customviews.R
+
+class ScalableActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scalable)
+    }
+
+}
